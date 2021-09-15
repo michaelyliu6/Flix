f2977dc77e18486306bbe6628a394e69e311af89
michaelyliu6 <michaelyliu6@gmail.com>
2021-09-14 22:36:14 -0400

    finished

---
+++ assignment1/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  assignment1
+//
+//  Created by Michael Liu on 9/5/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterLabel: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
